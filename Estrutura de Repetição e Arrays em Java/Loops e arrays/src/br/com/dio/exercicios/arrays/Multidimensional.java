162829913151dd005943a90599b62a2076495550
Hejociara <hejociara@gmail.com>
2022-01-19 11:28:05 -0300

    docs update

--- Estrutura de Repetição e Arrays em Java/Loops e arrays/src/br/com/dio/exercicios/arrays/Multidimensional.java
+++ Estrutura de Repetição e Arrays em Java/Loops e arrays/src/br/com/dio/exercicios/arrays/Multidimensional.java
@@ -7,15 +7,20 @@ public class Multidimensional {
         Random random = new Random();
 
         int [][] M = new int [4] [4];
+
         for(int i = 0; i < M.length; i++){
             for (int j = 0; j < M[i]. length; j++){
                 M[i][j] = random.nextInt(9);
             }
         }
+
+        System.out.println("Matriz: ");
         for ( int[] line : M ){
-            for(int columnElement : line){
+            for(int column : line){
+                System.out.print(column + " ");
 
             }
+            System.out.println();
         }
     }
 }
